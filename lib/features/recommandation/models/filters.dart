26f0035656cf7fe0a8769793abbf2139716c040f
Bochra Arfaoui <bochraarfaoui@insat.u-carthage.tn>
2023-01-15 21:34:23 +0100

    Add recommandation page

---
+++ lib/features/recommandation/models/filters.dart
@@ -0,0 +1,29 @@
+const List<bool> isAgeApproriateChoices = [true, false];
+const List<String> genresChoices = [
+  'Action & Adventure',
+  'Animation',
+  'Comedy',
+  'Crime',
+  'Documentary',
+  'Drama',
+  'Family',
+  'Kids',
+  'Mystery',
+  'News',
+  'Reality',
+  'Science-fiction & Fantastic',
+  'Soap',
+  'Talk',
+  'War & Politics',
+  'Western'
+];
+const List<String> languagesChoices = [
+  'English',
+  'Korean',
+  'French',
+  'Italian',
+  'Japanese',
+  'Spanish',
+  'German'
+];
+const List<String> durationChoices = ['<60mn', '60-120mn', '>120mn'];
